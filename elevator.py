f4d888cf09b4eb5a3267958bb80ed9186665319c
MatthieuPASCAL <matthieu.pascal.mp@gmail.com>
2024-11-04 16:28:59 +0100

    rien

--- elevator.py
+++ elevator.py
@@ -1,18 +1,22 @@
-#elevator.py
+# elevator.py
+from user import User
+
 
 class Elevator:
     def __init__(self, elevator_id, initial_floor=0):
         """
         Initialise un ascenseur avec un identifiant unique et un étage initial.
-        
+
         :param elevator_id: L'identifiant unique de l'ascenseur.
         :param initial_floor: L'étage initial de l'ascenseur (par défaut 0).
         """
-        self.id = elevator_id  #Identifiant unique de l'ascenseur.
-        self.current_floor = initial_floor  #Étage actuel de l'ascenseur.
-        self.destination_floors = []  #Liste des étages que l'ascenseur doit atteindre.
-        self.direction = 0  #Direction : -1 pour descendre, 1 pour monter, 0 pour inactif.
-        self.is_moving = False  #Statut de l'ascenseur (en mouvement ou non).
+        self.id = elevator_id  # Identifiant unique de l'ascenseur.
+        self.current_floor = initial_floor  # Étage actuel de l'ascenseur.
+        self.destination_floors = []  # Liste des étages que l'ascenseur doit atteindre.
+        self.direction = (
+            0  # Direction : -1 pour descendre, 1 pour monter, 0 pour inactif.
+        )
+        self.is_moving = False  # Statut de l'ascenseur (en mouvement ou non).
 
     def add_destination(self, floor):
         if floor not in self.destination_floors:
@@ -34,7 +38,11 @@ class Elevator:
                 self.destination_floors.pop(0)
                 self.direction = 0 if not self.destination_floors else self.direction
                 # Gérer les passagers qui descendent
-                departing_passengers = [p for p in self.passengers if p.destination_floor == self.current_floor]
+                departing_passengers = [
+                    p
+                    for p in self.passengers
+                    if p.destination_floor == self.current_floor
+                ]
                 for passenger in departing_passengers:
                     self.passengers.remove(passenger)
                     passenger.arrived = True
@@ -45,6 +53,7 @@ class Elevator:
 
     def __repr__(self):
         return f"Ascenseur {self.id} à l'étage {self.current_floor}"
+
 
 class ElevatorController:
     def __init__(self, num_elevators, num_floors):
