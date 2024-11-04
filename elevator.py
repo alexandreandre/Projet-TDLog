099c988b1621a78f4bd35df6ab63b1fb6f860d47
Louis Almairac <louis.almairac@orange.fr>
2024-11-04 15:50:41 +0100

    update

--- elevator.py
+++ elevator.py
@@ -19,3 +19,30 @@ class Elevator:
             self.destination_floors.append(floor)
             self.destination_floors.sort(reverse=self.direction < 0)
 
+    def move(self):
+        if self.destination_floors:
+            self.is_moving = True
+            next_floor = self.destination_floors[0]
+            if self.current_floor < next_floor:
+                self.current_floor += 1
+                self.direction = 1
+            elif self.current_floor > next_floor:
+                self.current_floor -= 1
+                self.direction = -1
+            else:
+                # Arrivé à l'étage
+                self.destination_floors.pop(0)
+                self.direction = 0 if not self.destination_floors else self.direction
+                # Gérer les passagers qui descendent
+                departing_passengers = [p for p in self.passengers if p.destination_floor == self.current_floor]
+                for passenger in departing_passengers:
+                    self.passengers.remove(passenger)
+                    passenger.arrived = True
+                # Vérifier si d'autres passagers montent
+        else:
+            self.is_moving = False
+            self.direction = 0
+
+    def __repr__(self):
+        return f"Ascenseur {self.id} à l'étage {self.current_floor}"
+
