3ebf23930a241569e7421da2aeda87f260f604bb
Alexandre André <alexandreandre2004@gmail.com>
2024-11-04 15:25:36 +0100

    mm

---
+++ elevator.py
@@ -0,0 +1,13 @@
+class Elevator:
+    def __init__(self, elevator_id, initial_floor=0):
+        """
+        Initialise un ascenseur avec un identifiant unique et un étage initial.
+        
+        :param elevator_id: L'identifiant unique de l'ascenseur.
+        :param initial_floor: L'étage initial de l'ascenseur (par défaut 0).
+        """
+        self.id = elevator_id  #Identifiant unique de l'ascenseur.
+        self.current_floor = initial_floor  #Étage actuel de l'ascenseur.
+        self.destination_floors = []  #Liste des étages que l'ascenseur doit atteindre.
+        self.direction = 0  #Direction : -1 pour descendre, 1 pour monter, 0 pour inactif.
+        self.is_moving = False  #Statut de l'ascenseur (en mouvement ou non).
